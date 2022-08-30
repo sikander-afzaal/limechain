20d504932d82c869d2239664504130c347a58092
sikander-afzaal <sikander.aaa@gmail.com>
2022-08-30 11:56:05 +0500

    changes 1

--- src/Components/ServiceBox.jsx
+++ src/Components/ServiceBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function ServiceBox({ text }) {
   return (
-    <div className="flex justify-between items-start w-[275px] h-[130px] rounded p-5 flex-col bg-main text-white">
+    <div className="flex w-full justify-between items-start md:w-[275px] h-[130px] rounded p-5 flex-col bg-main text-white">
       <div className="w-[30%] h-[2px] bg-white"></div>
       <p className="text-lg">{text}</p>
     </div>
