32c7fcf6a15dbdb72401375cdad7983ea41fc711
sikander-afzaal <sikander.aaa@gmail.com>
2023-02-03 19:23:36 +0500

    asdasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Index from "./Pages/Landing/Index";
 import Metaverse from "./Pages/Metaverse/Metaverse";
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="App">
       <Routes>
